dfc24e7dffac4e61dbf32b62434e612b09f13c1c
Povilas Sauciuvienas <p.sauciuvienas@gmail.com>
2026-06-22 11:14:52 +0100

    src structure for aev_plig

--- create_pytorch_data.py
+++ create_pytorch_data.py
@@ -1,67 +1,47 @@
 import pandas as pd
 import pickle
-from utils import GraphDataset
+from aev_plig.create_pytorch_data import GraphDataset
 
-"""
-Load graphs
-"""
-print("loading graph from pickle file for pdbbind2020")
-with open("data/pdbbind.pickle", 'rb') as handle:
-    pdbbind_graphs = pickle.load(handle)
+if __name__ == "__main__":
+    print("loading graph from pickle file for pdbbind2020")
+    with open("data/pdbbind.pickle", 'rb') as handle:
+        pdbbind_graphs = pickle.load(handle)
 
-print("loading graph from pickle file for BindingNet")
-with open("data/bindingnet.pickle", 'rb') as handle:
-    bindingnet_graphs = pickle.load(handle)
+    print("loading graph from pickle file for BindingNet")
+    with open("data/bindingnet.pickle", 'rb') as handle:
+        bindingnet_graphs = pickle.load(handle)
 
-print("loading graph from pickle file for BindingDB")
-with open("data/bindingdb.pickle", 'rb') as handle:
-    bindingdb_graphs = pickle.load(handle)
+    print("loading graph from pickle file for BindingDB")
+    with open("data/bindingdb.pickle", 'rb') as handle:
+        bindingdb_graphs = pickle.load(handle)
 
-graphs_dict = {**pdbbind_graphs, **bindingnet_graphs, **bindingdb_graphs}
+    graphs_dict = {**pdbbind_graphs, **bindingnet_graphs, **bindingdb_graphs}
 
+    pdbbind = pd.read_csv("data/pdbbind_processed.csv", index_col=0)
+    pdbbind = pdbbind[['PDB_code', '-logKd/Ki', 'split_core', 'max_tanimoto_fep_benchmark']]
+    pdbbind = pdbbind.rename(columns={'PDB_code': 'unique_id', 'split_core': 'split', '-logKd/Ki': 'pK'})
+    pdbbind = pdbbind[pdbbind["max_tanimoto_fep_benchmark"] < 0.9]
+    pdbbind = pdbbind[['unique_id', 'pK', 'split']]
 
-"""
-Generate data for enriched training for <0.9 Tanimoto to the FEP benchmark
-"""
-pdbbind = pd.read_csv("data/pdbbind_processed.csv", index_col=0)
-pdbbind = pdbbind[['PDB_code','-logKd/Ki','split_core','max_tanimoto_fep_benchmark']]
-pdbbind = pdbbind.rename(columns={'PDB_code':'unique_id', 'split_core':'split', '-logKd/Ki':'pK'})
-pdbbind = pdbbind[pdbbind["max_tanimoto_fep_benchmark"] < 0.9]
-pdbbind = pdbbind[['unique_id','pK','split']]
+    bindingnet = pd.read_csv("data/bindingnet_processed.csv", index_col=0)
+    bindingnet = bindingnet.rename(columns={'-logAffi': 'pK', 'unique_identify': 'unique_id'})[['unique_id', 'pK', 'max_tanimoto_fep_benchmark']]
+    bindingnet['split'] = 'train'
+    bindingnet = bindingnet[bindingnet["max_tanimoto_fep_benchmark"] < 0.9]
+    bindingnet = bindingnet[['unique_id', 'pK', 'split']]
 
-bindingnet = pd.read_csv("data/bindingnet_processed.csv", index_col=0)
-bindingnet = bindingnet.rename(columns={'-logAffi': 'pK','unique_identify':'unique_id'})[['unique_id','pK','max_tanimoto_fep_benchmark']]
-bindingnet['split'] = 'train'
-bindingnet = bindingnet[bindingnet["max_tanimoto_fep_benchmark"] < 0.9]
-bindingnet = bindingnet[['unique_id','pK','split']]
+    bindingdb = pd.read_csv("data/bindingdb_processed.csv", index_col=0)
+    bindingdb = bindingdb[['unique_id', 'pK', 'max_tanimoto_fep_benchmark']]
+    bindingdb['split'] = 'train'
+    bindingdb = bindingdb[bindingdb["max_tanimoto_fep_benchmark"] < 0.9]
+    bindingdb = bindingdb[['unique_id', 'pK', 'split']]
 
-bindingdb = pd.read_csv("data/bindingdb_processed.csv", index_col=0)
-bindingdb = bindingdb[['unique_id','pK','max_tanimoto_fep_benchmark']]
-bindingdb['split'] = 'train'
-bindingdb = bindingdb[bindingdb["max_tanimoto_fep_benchmark"] < 0.9]
-bindingdb = bindingdb[['unique_id','pK','split']]
+    data = pd.concat([pdbbind, bindingnet, bindingdb], ignore_index=True)
+    print(data[['split']].value_counts())
 
-# combine pdbbind2020, bindingnet, and bindingdb index sets
-data = pd.concat([pdbbind, bindingnet, bindingdb], ignore_index=True)
-print(data[['split']].value_counts())
+    dataset = 'pdbbind_U_bindingnet_U_bindingdb_ligsim90_fep_benchmark'
 
-dataset = 'pdbbind_U_bindingnet_U_bindingdb_ligsim90_fep_benchmark'
-
-df = data[data['split'] == 'train']
-train_ids, train_y = list(df['unique_id']), list(df['pK'])
-
-df = data[data['split'] == 'valid']
-valid_ids, valid_y = list(df['unique_id']), list(df['pK'])
-
-df = data[data['split'] == 'test']
-test_ids, test_y = list(df['unique_id']), list(df['pK'])
-
-# make data PyTorch Geometric ready
-print('preparing ', dataset + '_train.pt in pytorch format!')
-train_data = GraphDataset(root='data', dataset=dataset + '_train', ids=train_ids, y=train_y, graphs_dict=graphs_dict)
-
-print('preparing ', dataset + '_valid.pt in pytorch format!')
-valid_data = GraphDataset(root='data', dataset=dataset + '_valid', ids=valid_ids, y=valid_y, graphs_dict=graphs_dict)
-
-print('preparing ', dataset + '_test.pt in pytorch format!')
-test_data = GraphDataset(root='data', dataset=dataset + '_test', ids=test_ids, y=test_y, graphs_dict=graphs_dict)
+    for split in ['train', 'valid', 'test']:
+        df = data[data['split'] == split]
+        ids, y = list(df['unique_id']), list(df['pK'])
+        print(f'preparing {dataset}_{split}.pt in pytorch format!')
+        GraphDataset(root='data', dataset=f'{dataset}_{split}', ids=ids, y=y, graphs_dict=graphs_dict)
---
+++ src/aev_plig/create_pytorch_data.py
@@ -0,0 +1,141 @@
+import pandas as pd
+import pickle
+import os
+import argparse
+
+from torch_geometric.data import InMemoryDataset, Data
+from aev_plig_local.utils import GraphDataset as BaseGraphDataset
+import torch
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+
+def load_graphs_from_folder(folder: str = "data/aev_plig/graphs"):
+    """Load pickled graphs from a folder"""
+    files = [
+        file
+        for file in os.listdir(folder)
+        if file.endswith(".pickle") or file.endswith(".pkl")
+    ]
+    if not files:
+        raise ValueError(f"No pickle files found in folder {folder}")
+    graphs = {}
+
+    for file in files:
+        with open(os.path.join(folder, file), "rb") as handle:
+            graphs.update(pickle.load(handle))
+    if not graphs:
+        raise ValueError(f"Pickling failed to load any graphs from folder {folder}")
+    return graphs
+
+
+class GraphDataset(BaseGraphDataset):
+    def __init__(
+        self,
+        root="data",
+        dataset=None,
+        ids=None,
+        y=None,
+        graphs_dict=None,
+        y_scaler=None,
+        regenerate=True,
+        **kwargs,
+    ):
+        super(GraphDataset, self).__init__(root, dataset, ids, y, graphs_dict, y_scaler)
+
+        torch.serialization.add_safe_globals([Data])
+        if not regenerate and os.path.isfile(self.processed_paths[0]):
+            self.load(self.processed_paths[0])
+            print("processed paths:")
+            print(self.processed_paths[0])
+        else:
+            self.process(ids, y, graphs_dict)
+            self.load(self.processed_paths[0])
+
+        if y_scaler is None:
+            y_scaler = StandardScaler()
+            y_scaler.fit(np.reshape(self._data.y, (self.__len__(), 1)))
+        self.y_scaler = y_scaler
+        self._data.y = [torch.tensor(element[0]).float() for element in self.y_scaler.transform(np.reshape(self._data.y, (self.__len__(), 1)))]
+
+    def process(self, ids, y, graphs_dict):
+        assert len(ids) == len(y), "Number of datapoints and labels must be the same"
+        data_list = []
+        for i in range(len(ids)):
+            pdbcode = ids[i]
+            label = y[i]
+            _, features, edge_index, edge_features = graphs_dict[pdbcode]
+
+            data_point = Data(
+                x=torch.Tensor(np.array(features)),
+                edge_index=torch.LongTensor(np.array(edge_index)).T,
+                edge_attr=torch.Tensor(np.array(edge_features)),
+                y=torch.FloatTensor(np.array([label])),
+            )
+            data_list.append(data_point)
+
+        print("Graph construction done. Saving to file.")
+        self.save(data_list, self.processed_paths[0])
+
+
+def save_splits(
+    df: pd.DataFrame,
+    name: str,
+    output_folder: str = "data",
+    regenerate: bool = False,
+) -> None:
+    """Save train/valid/test splits in PyTorch format"""
+    graphs_dict = load_graphs_from_folder()
+
+    for split in ["train", "valid", "test"]:
+        if split not in df["split"].values:
+            print(f"Warning: split '{split}' not found in DataFrame. Skipping.")
+            continue
+
+        graph_name = f"{name}_{split}"
+        print(f"preparing {graph_name} in pytorch format!")
+        split_df = df[df["split"] == split]
+        missing_samples = split_df[~split_df["unique_id"].isin(graphs_dict.keys())]
+        split_df = split_df[split_df["unique_id"].isin(graphs_dict.keys())]
+
+        ids, y = list(split_df["unique_id"]), list(split_df["pK"])
+        GraphDataset(
+            root=output_folder,
+            dataset=graph_name,
+            ids=ids,
+            y=y,
+            graphs_dict=graphs_dict,
+            regenerate=regenerate,
+        )
+
+        print(f"Saved PyTorch datasets to {graph_name}")
+        print(f"Finished processing {graph_name} of length {len(split_df)}")
+        if not missing_samples.empty:
+            print(
+                f"Warning: {len(missing_samples)} samples were missing from the graphs dictionary and were skipped."
+            )
+            print(missing_samples["unique_id"].tolist())
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create PyTorch datasets for AEV-PLIG")
+    parser.add_argument(
+        "--csv_path", type=str, default="datasets/raw_datasets/bindingdb_processed.csv"
+    )
+    parser.add_argument("--output_name", type=str, default="trial_run")
+    parser.add_argument("--output_folder", type=str, default="data")
+    parser.add_argument(
+        "--no_regenerate",
+        action="store_true",
+        help="Do not regenerate the datasets even if they already exist",
+    )
+    args = parser.parse_args()
+
+    training_data = pd.read_csv(args.csv_path)
+    save_splits(
+        training_data,
+        args.output_name,
+        output_folder=args.output_folder,
+        regenerate=not args.no_regenerate,
+    )
+    print("Done!")
